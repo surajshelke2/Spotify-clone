426fb32923ee32aec2b96302e64f35c5ad15bffc
Suraj Shelke <surajshelke332@gmail.com>
2024-06-22 20:52:46 +0530

    adding bg color

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,11 +1,34 @@
-import { Route, Routes } from "react-router-dom"
+import { Route, Routes, useLocation } from "react-router-dom"
 import DisplayHome from "./DisplayHome"
 import DisplayAlbum from "./DisplayAlbum"
+import { useEffect, useRef } from "react"
+import { albumsData } from "../assets/assets"
 
 
-const Display = () => {
+const Display:React.FC = () => {
+
+  const displayRef = useRef<HTMLDivElement | null>(null);
+
+    const location = useLocation();
+    const isAlbum = location.pathname.includes('album');
+    const albumId = isAlbum?location.pathname.slice(-1):"";
+    const bgColor = albumsData[Number(albumId)].bgColor;
+
+    useEffect(() => {
+      
+      if (displayRef.current) {
+        if (isAlbum) {
+          displayRef.current.style.background = `linear-gradient(to bottom, ${bgColor}, #000000)`;
+        } else {
+          displayRef.current.style.background = 'linear-gradient(to bottom, #000000, #000000)';
+        }
+      }
+     
+    })
+    
+
   return (
-    <div className='w-[100%] m-2 px-6 pt-4 rounded  bg-[#121212] text-white overflow-auto lg:w-[75%] '>
+    <div  ref={displayRef} className='w-[100%] m-2 px-6 pt-4 rounded  bg-[#121212] text-white overflow-auto lg:w-[75%] '>
       <Routes>
         <Route path="/" element={<DisplayHome/>} />
         <Route path="/album/:id" element={<DisplayAlbum/>}></Route>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,52 @@
-
-import { assets } from '../assets/assets'
+import { useNavigate } from "react-router-dom";
+import { assets } from "../assets/assets";
 
 const Navbar = () => {
+  const Navigate = useNavigate();
   return (
     <>
       <div className="w-full flex justify-between font-semibold">
-        <div className="flex items-center gap-2">
-        <img className='w-4 bg-black rounded-2xl cursor-pointer' src={assets.arrow_left} alt="" />
-        <img className='w-4 bg-black rounded-2xl cursor-pointer' src={assets.arrow_right} alt="" />
+        <div className="flex items-center gap-4">
+          <div
+            className="bg-black rounded-2xl p-2 cursor-pointer"
+            onClick={() => Navigate(-1)}
+          >
+            <img className="w-4 h-4" src={assets.arrow_left} alt="Left Arrow" />
+          </div>
+          <div
+            className="bg-black rounded-2xl p-2 cursor-pointer"
+            onClick={() => Navigate(+1)}
+          >
+            <img
+              className="w-4 h-4"
+              src={assets.arrow_right}
+              alt="Right Arrow"
+            />
+          </div>
         </div>
         <div className="flex items-center gap-4">
-            <p className='bg-white text-black text-[15px] px-4 py-1 rounded-2xl hidden md:block cursor-pointer'>Explore Premium</p>
-            <p className="bg-black py-1 px-3 rounded-2xl text-[15px] cursor-pointer">Install App</p>
-            <p className='bg-purple-500 text-black w-8 h-8 rounded-full items-center font-bold text-center justify-center'>A</p>
+          <p className="bg-white text-black text-[15px] px-4 py-1 rounded-2xl hidden md:block cursor-pointer">
+            Explore Premium
+          </p>
+          <p className="bg-black py-1 px-3 rounded-2xl text-[15px] cursor-pointer">
+            Install App
+          </p>
+          <p className="bg-purple-500 text-black w-8 h-8 rounded-full items-center font-bold text-center justify-center">
+            A
+          </p>
         </div>
       </div>
       <div className="flex items-center gap-2 mt-4">
-        <p className='bg-white text-black px-4 py-1 rounded-2xl cursor-pointer'>All</p>
-        <p className=' bg-black px-4 py-1 rounded-2xl cursor-pointer'>Music</p>
-        <p className=' bg-black px-4 py-1 rounded-2xl cursor-pointer'>Podcast</p>
-       
+        <p className="bg-white text-black px-4 py-1 rounded-2xl cursor-pointer">
+          All
+        </p>
+        <p className=" bg-black px-4 py-1 rounded-2xl cursor-pointer">Music</p>
+        <p className=" bg-black px-4 py-1 rounded-2xl cursor-pointer">
+          Podcast
+        </p>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
